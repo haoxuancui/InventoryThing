d61810903b1ac526a1e35ba186be0872ac442d53
Haoxuan Cui <haoxuan.cui@mullenmail.com>
2018-02-14 08:53:49 -0700

    wednesday

--- Inventory Thing/src/employee.java
+++ Inventory Thing/src/employee.java
@@ -1,49 +1,43 @@
 import java.util.*;
 import java.io.*;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.text.*;
 public class employee
 	{
 		static ArrayList<Game> stock = new ArrayList<Game>();// stock
 		private static int userChoice;
-		public static void main(String[] args)
-			{
-				
-				Scanner file = new Scanner(new File("HereItIs.txt"));
-				while(file.hasNextLine())
-					{
-						String list =file.nextLine();
-						String [] reallist= list.split(" ");
-						stock.add(new Game(reallist[0],reallist[1],reallist[2],reallist[3],reallist[4],reallist[5],reallist[6]));
-					} 
-				
-				System.out.println("Hi, here is the stock for us.");
-				for(int i=0; i<stock.size();i++)
-					{
-						System.out.println(Games.roster.get(i).getName()+Games.roster.get(i).getSkus()+Games.roster.get(i).getConsole()+Games.roster.get(i).getWhole()+Games.roster.get(i).getSell());
-					}
-				
+		private static boolean keepAsking=true;
+		public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException
+		{
 				System.out.println("What do you want to do?");
 				System.out.println("1. Buy Stock");
 				System.out.println("2. Exit the website");
-				Scanner userInput = new Scanner(System.in);
-				userChoice = userInput.nextInt();
-				if(userChoice==1)
+				while(keepAsking) 
 					{
-						buyStock();
-					}
-				else if(userChoice
-						==2)
-					{
-						System.out.println("Bye bye!!!");
+						Scanner userInput = new Scanner(System.in);
+						userChoice = userInput.nextInt();
+						if(userChoice==1)
+							{
+								buyStock();
+								break;
+							}
+						else if(userChoice==2)
+							{
+								System.out.println("Bye bye!!!");
+								break;
+							}
+						else
+							{
+								System.out.println("Try it again");
+							}
 					}
 				
-			}
-		private static void buyStock()
-			{
 				
 			}
+		private static void buyStock() throws FileNotFoundException, UnsupportedEncodingException
+		{
+			PrintWriter writer = new PrintWriter("Inventory.txt", "UTF-8");
+			writer.newLine();
+			writer.close();
+		}
 
 	}
